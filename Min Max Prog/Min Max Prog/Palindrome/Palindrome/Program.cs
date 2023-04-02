8a963790448f0ffaf42c4c68b0af7cce8258f6a9
eagleten <107332489+eagleten@users.noreply.github.com>
2023-04-02 15:56:22 -0400

    Add files via upload

---
+++ Min Max Prog/Min Max Prog/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+
+namespace Palindrome
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 0, j, flag = 0;
+            char[] arr;
+            Console.WriteLine("Enter string");
+            arr = Console.ReadLine().ToCharArray();
+            j = arr.Length - 1;
+            while (i <= j)
+            {
+                if (arr[i] == arr[j])
+                {
+                    i++;
+                    j--;
+
+                }
+                else
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Entered string is a palidrome");
+
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a palidrome");
+            }
+        }
+    }
+
+}
