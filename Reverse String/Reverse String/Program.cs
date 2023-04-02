cfa4c275c8d17c9c5408b22b013e29959b8bc7a7
eagleten <107332489+eagleten@users.noreply.github.com>
2023-04-02 16:09:39 -0400

    Add files via upload

---
+++ Reverse String/Reverse String/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+namespace ReverseString
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string[] arr = { "C", "s", "h", "r", "a", "p", };
+            int i = 5;
+            Console.WriteLine("The reverse of the string is:");
+            while(i >= 0)
+            {
+                Console.Write(arr[i]);
+                i--;
+            }
+            Console.Read();
+
+        }
+    }
+
+}
