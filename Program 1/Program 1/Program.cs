10059df867e2de6c43a6bfd32cc15bfd83d44531
eagleten <107332489+eagleten@users.noreply.github.com>
2023-04-02 16:33:21 -0400

    Add files via upload

---
+++ Program 1/Program 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Program1
+{
+    class Employee
+
+    {
+        string name, email;
+        int phoneno;
+
+        static void Main(string[] args)
+        {
+            Employee maria = new Employee();
+            maria.name = "Maria Pontes";
+            maria.email = "maria@gmail.com";
+            maria.phoneno = 12345;
+            Console.WriteLine("{0}", maria.name);
+            Console.WriteLine("{0}", maria.email);
+            Console.WriteLine("{0}", maria.phoneno);
+             
+
+        }
+    }
+
+}
