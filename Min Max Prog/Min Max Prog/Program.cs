eb296b38ed5cf4f95da723cbd726430d82426636
eagleten <107332489+eagleten@users.noreply.github.com>
2023-04-02 15:54:31 -0400

    Add files via upload

---
+++ Min Max Prog/Min Max Prog/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace MinMaxProg
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string str_num = "2 7 5 6 1";
+            Console.WriteLine("Input: " + str_num);
+            Console.WriteLine("Output: " +test(str_num));
+            
+        }
+        public static string test(string str_num)
+        {
+            var result = str_num.Split(' ').Select(int.Parse).ToArray();
+            return result.Max() + " " + result.Min();
+        }
+    }
+}
